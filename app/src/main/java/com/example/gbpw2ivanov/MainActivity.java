e763b21903c00211c82bf4de7c082dcacf705a6e
aleksivanov <ivanov.aleksey.job@gmail.com>
2022-04-02 12:31:46 +1000

    в strings заменил запятую на точку, т.к. запятая не считывается как точка в activity_main добавил недостающую реализацию кнопок в activity_main.xml добавил второй монитор для более удобного вывода информации по действиям

--- app/src/main/java/com/example/gbpw2ivanov/MainActivity.java
+++ app/src/main/java/com/example/gbpw2ivanov/MainActivity.java
@@ -11,17 +11,19 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix, buttonComma;
+    private Button buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix, buttonDot;
     private Button buttonSeven, buttonEight, buttonNine, buttonZero, buttonClear, buttonBack;
     private Button buttonPercent, buttonDiv, buttonMulti, buttonMinus, buttonPlus, buttonEqual;
-    private TextView monitor;
-    private String stringValueOne = "";
+    private TextView monitor, monitor_2;
     private int intValueOne;
-    private String stringValueTwo = "";
     private int intValueTwo;
+    private double doubleValueOne;
+    private double doubleValueTwo;
     private int intResult;
-    private String stringResult;
+    private double doubleResult;
     private String operation;
+    private boolean isComma = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         buttonEight = findViewById(R.id.buttonEight);
         buttonNine = findViewById(R.id.buttonNine);
         buttonZero = findViewById(R.id.buttonZero);
-        buttonComma = findViewById(R.id.buttonComma);
+        buttonDot = findViewById(R.id.buttonDot);
         buttonClear = findViewById(R.id.buttonClear);
         buttonBack = findViewById(R.id.buttonBack);
         buttonPercent = findViewById(R.id.percent);
@@ -48,184 +50,253 @@ public class MainActivity extends AppCompatActivity {
         buttonPlus = findViewById(R.id.buttonPlus);
         buttonEqual = findViewById(R.id.buttonEqual);
         monitor = findViewById(R.id.monitor);
-
-
+        monitor_2 = findViewById(R.id.monitor_2);
 
 
         buttonOne.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonOne.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonOne.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonOne.getText());
         });
 
 
         buttonTwo.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonTwo.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonTwo.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonTwo.getText());
         });
 
         buttonThree.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonThree.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonThree.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonThree.getText());
         });
 
         buttonFour.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonFour.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonFour.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonFour.getText());
         });
 
         buttonFive.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonFive.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonFive.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonFive.getText());
         });
 
         buttonSix.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonSix.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonSix.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonSix.getText());
         });
 
         buttonSeven.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonSeven.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonSeven.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonSeven.getText());
         });
 
         buttonEight.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonEight.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonEight.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonEight.getText());
         });
 
         buttonNine.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonNine.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonNine.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonNine.getText());
         });
 
         buttonZero.setOnClickListener(view -> {
-            if (intValueOne > 0) {
-                stringValueTwo += (String) buttonZero.getText();
-                monitor.setText(stringValueTwo);
-            } else {
-                stringValueOne += (String) buttonZero.getText();
-                monitor.setText(stringValueOne);
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
             }
+            monitor_2.append(buttonZero.getText());
         });
 
-
-
         buttonPlus.setOnClickListener(view -> {
-            intValueOne = Integer.parseInt(String.valueOf(monitor.getText()));
-            monitor.setText(null);
-            operation = "+";
+//            if (isComma) {
+//                monitor.append(monitor_2.getText() + "+");
+//                doubleValueOne = Double.parseDouble(String.valueOf(monitor_2.getText()));
+//                monitor_2.setText(null);
+//                operation = "+";
+//            } else {
+                monitor.append(monitor_2.getText() + "+");
+                intValueOne = Integer.parseInt(String.valueOf(monitor_2.getText()));
+                monitor_2.setText(null);
+                operation = "+";
+//            }
         });
 
         buttonMinus.setOnClickListener(view -> {
-            intValueOne = Integer.parseInt(String.valueOf(monitor.getText()));
-            monitor.setText(null);
-            operation = "-";
+//            if (isComma) {
+//                monitor.append(monitor_2.getText() + "-");
+//                doubleValueOne = Double.parseDouble(String.valueOf(monitor_2.getText()));
+//                monitor_2.setText(null);
+//                operation = "-";
+//            } else {
+                monitor.append(monitor_2.getText() + "-");
+                intValueOne = Integer.parseInt(String.valueOf(monitor_2.getText()));
+                monitor_2.setText(null);
+                operation = "-";
+//            }
         });
 
         buttonDiv.setOnClickListener(view -> {
-            intValueOne = Integer.parseInt(String.valueOf(monitor.getText()));
-            monitor.setText(null);
-            operation = "÷";
+//            if (isComma) {
+//                monitor.append(monitor_2.getText() + "÷");
+//                doubleValueOne = Double.parseDouble(String.valueOf(monitor_2.getText()));
+//                monitor_2.setText(null);
+//                operation = "÷";
+//            } else {
+                monitor.append(monitor_2.getText() + "÷");
+                intValueOne = Integer.parseInt(String.valueOf(monitor_2.getText()));
+                monitor_2.setText(null);
+                operation = "÷";
+//            }
         });
 
         buttonMulti.setOnClickListener(view -> {
-            intValueOne = Integer.parseInt(String.valueOf(monitor.getText()));
-            monitor.setText(null);
-            operation = "*";
+//            if (isComma) {
+//                monitor.append(monitor_2.getText() + "*");
+//                doubleValueOne = Double.parseDouble(String.valueOf(monitor_2.getText()));
+//                monitor_2.setText(null);
+//                operation = "*";
+//            } else {
+                monitor.append(monitor_2.getText() + "*");
+                intValueOne = Integer.parseInt(String.valueOf(monitor_2.getText()));
+                monitor_2.setText(null);
+                operation = "*";
+//            }
         });
 
         buttonClear.setOnClickListener(view -> {
+            monitor_2.setText(null);
             monitor.setText(null);
             intValueOne = 0;
             intValueTwo = 0;
-            stringValueOne = "";
-            stringValueTwo = "";
+        });
+
+        buttonBack.setOnClickListener(view -> {
+            StringBuilder st = new StringBuilder(monitor_2.getText());
+            st.deleteCharAt(st.length() - 1);
+            monitor_2.setText(st.toString());
+        });
+
+        buttonDot.setOnClickListener(view -> {
+            if (operation == "=") {
+                monitor_2.setText(null);
+                monitor.setText(null);
+                operation = "";
+            }
+            monitor_2.append(buttonDot.getText());
+            isComma = true;
         });
 
 
         buttonEqual.setOnClickListener(view -> {
+//            if (isComma) {
+//                switch (operation) {
+//                    case "+":
+//                        doubleValueTwo = Double.parseDouble(String.valueOf(monitor_2.getText()));
+//                        doubleResult = doubleValueOne + doubleValueTwo;
+//                        monitor_2.setText(String.valueOf(doubleResult));
+//                        monitor.setText(monitor.getText() + String.valueOf(doubleValueTwo) + "=");
+//                        break;
+//
+//                    case "-":
+//                        doubleValueTwo = Double.parseDouble(String.valueOf(monitor_2.getText()));
+//                        doubleResult = doubleValueOne - doubleValueTwo;
+//                        monitor_2.setText(String.valueOf(doubleResult));
+//                        monitor.setText(monitor.getText() + String.valueOf(doubleValueTwo) + "=");
+//                        break;
+//
+//                    case "÷":
+//                        doubleValueTwo = Double.parseDouble(String.valueOf(monitor_2.getText()));
+//                        doubleResult = doubleValueOne / doubleValueTwo;
+//                        monitor_2.setText(String.valueOf(doubleResult));
+//                        monitor.setText(monitor.getText() + String.valueOf(doubleValueTwo) + "=");
+//
+//                    case "*":
+//                        doubleValueTwo = Double.parseDouble(String.valueOf(monitor_2.getText()));
+//                        doubleResult = doubleValueOne * doubleValueTwo;
+//                        monitor_2.setText(String.valueOf(doubleResult));
+//                        monitor.setText(monitor.getText() + String.valueOf(doubleValueTwo) + "=");
+//                }
+//
+//            } else {
+                switch (operation) {
+                    case "+":
+                        intValueTwo = Integer.parseInt(String.valueOf(monitor_2.getText()));
+                        intResult = intValueOne + intValueTwo;
+                        monitor_2.setText(String.valueOf(intResult));
+                        monitor.setText(monitor.getText() + String.valueOf(intValueTwo) + "=");
+                        break;
 
-            switch (operation) {
-                case "+":
-                    intValueTwo = Integer.parseInt(String.valueOf(monitor.getText()));
-                    intResult = intValueOne + intValueTwo;
-                    stringResult = String.valueOf(intResult);
-                    monitor.setText(stringResult);
-                    break;
+                    case "-":
+                        intValueTwo = Integer.parseInt(String.valueOf(monitor_2.getText()));
+                        intResult = intValueOne - intValueTwo;
+                        monitor_2.setText(String.valueOf(intResult));
+                        monitor.setText(monitor.getText() + String.valueOf(intValueTwo) + "=");
+                        break;
 
-                case "-":
-                    intValueTwo = Integer.parseInt(String.valueOf(monitor.getText()));
-                    intResult = intValueOne - intValueTwo;
-                    stringResult = String.valueOf(intResult);
-                    monitor.setText(stringResult);
-                    break;
+                    case "÷":
+                        intValueTwo = Integer.parseInt(String.valueOf(monitor_2.getText()));
+                        intResult = intValueOne / intValueTwo;
+                        monitor_2.setText(String.valueOf(intResult));
+                        monitor.setText(monitor.getText() + String.valueOf(intValueTwo) + "=");
+                        break;
 
-                case "÷":
-                    intValueTwo = Integer.parseInt(String.valueOf(monitor.getText()));
-                    intResult = intValueOne / intValueTwo;
-                    stringResult = String.valueOf(intResult);
-                    monitor.setText(stringResult);
-                    break;
+                    case "*":
+                        intValueTwo = Integer.parseInt(String.valueOf(monitor_2.getText()));
+                        intResult = intValueOne * intValueTwo;
+                        monitor_2.setText(String.valueOf(intResult));
+                        monitor.setText(monitor.getText() + String.valueOf(intValueTwo) + "=");
+                        break;
+                }
+//            }
 
-                case "*":
-                    intValueTwo = Integer.parseInt(String.valueOf(monitor.getText()));
-                    intResult = intValueOne * intValueTwo;
-                    stringResult = String.valueOf(intResult);
-                    monitor.setText(stringResult);
-                    break;
-            }
+            operation = "=";
+            isComma = false;
 
             intValueOne = 0;
             intValueTwo = 0;
-            stringValueOne = "";
-            stringValueTwo = "";
+            doubleValueOne = 0;
+            doubleValueTwo = 0;
         });
 
 
+
     }
+
 }
